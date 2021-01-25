4bc4ac59b600697644b2f4aced3b07d0edcc77a7
Pavel-Yablonski <yablonski.pavel81@gmail.com>
2021-01-26 01:37:39 +0300

    init: reverse-int task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    n = String(n).split("");
+
+    for (let i = 0; i < n.length; i++) {
+        if (n[0] == "-") {
+            n = n.splice(1, n.length);
+        }
+    }
+
+    return Number(n.reverse().reduce((acum, next) => acum + next));
 }
